faffc4e15ac618d5517642461f85b5bee1983c81
BenjeeDaBoi <bha9465@students.fhv.at>
2021-12-30 02:03:13 +0100

    Solution Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# AoC 2021 - Day 1
+
+This is a possible solution to [the 1st Day`s problem](https://adventofcode.com/2021/day/1)  of **Advent of Code 2021**.
+Written in Python 3, as a challenge to learn a new language.
+
+### Disclaimer
+
+Solutions might not be optimal. Solving those puzzles are for fun and to learn new things.
+They do not need use the most optimal code or algorithm.
+The goal is to solve the problems somewhat efficiently and to have fun.
---
+++ day1_sonar_sweep.py
@@ -0,0 +1,48 @@
+# # # # # # # # # # # # # # # # # # 
+# # # # Advent of Code 2021 # # # #
+# # # - - - - - - - - - - - - # # #
+# # # #   Code by Benjee    # # # # 
+# # # # # # # # # # # # # # # # # #
+# #  Solutions are individual   # #
+# # # # # # # # # # # # # # # # # #
+
+
+import os
+
+def readMeasures():
+    
+    file = open(os.path.join(os.getcwd(), os.path.dirname(__file__), "input/puzzle_input.txt"), "r")
+    depthMeasures = []
+    
+    for num in file:
+        depthMeasures.append(int(num))
+        
+    file.close()
+    return depthMeasures
+
+def calcDepthMeasureIncrements(depthMeasures):
+    
+    depthIncreases = 0
+    for i in range(1, len(depthMeasures)):
+        if (depthMeasures[i] > depthMeasures[i-1]):
+            depthIncreases = depthIncreases + 1
+            
+    return depthIncreases
+
+def calcThreeWaysDepthMeasureIncrements(depthMeasures):
+    
+    newDepthMeasures = []
+    depthIncreases = 0
+    for i in range(0, len(depthMeasures) - 2):
+        newDepthMeasures.append(depthMeasures[i] + depthMeasures[i+1] + depthMeasures[i+2])
+        if ((i > 0) and (newDepthMeasures[i] > newDepthMeasures[i-1])):
+            depthIncreases = depthIncreases + 1
+
+    return depthIncreases
+
+
+# Execute Solution
+
+depthMeasures = readMeasures()
+print(f"The amount of Depth Measure Increases of the Test Sample is {calcDepthMeasureIncrements(depthMeasures)}")
+print(f"The amount of Triple Depth Measure Increases of the Test Sample is {calcThreeWaysDepthMeasureIncrements(depthMeasures)}")
